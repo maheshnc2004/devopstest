aa18381c632d8fbd1b8b17ac123f913644b55426
maheshnc2004 <110278357+maheshnc2004@users.noreply.github.com>
2025-03-24 14:50:41 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -8,11 +8,11 @@ class demo{
 
 		if (num % 2 == 0) {
 
-			System.out.println("Entered Number is Even");
+			System.out.println("Number is Even");
 		}
 
 		else {
-			System.out.println("Entered Number is Odd");
+			System.out.println("Number is Odd");
 		}
 	}
 }
